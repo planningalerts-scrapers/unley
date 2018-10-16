c05ce5a05cf59737ae63c0c1f6a02523314e23c5
MichaelBone <37421067+MichaelBone@users.noreply.github.com>
2018-10-16 18:08:27 +1030

    Changed to use a proxy.

--- scraper.rb
+++ scraper.rb
@@ -10,7 +10,9 @@ agent = Mechanize.new do |a|
   a.keep_alive = true
   # a.log = Logger.new $stderr
   # a.agent.http.debug_output = $stderr
-  a.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  # a.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  host, port = ENV['MORPH_PROXY'].split(":")
+  a.set_proxy(host, port)
 end
 
 p "Getting first page"
