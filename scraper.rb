cbaf8013791c76ce98b073110a1e857fdeaa612c
Matthew Landauer <mlandauer@gmail.com>
2019-06-05 12:10:09 +1000

    Upgrade to latest version of the gem and add missing state to the end of the address

--- scraper.rb
+++ scraper.rb
@@ -2,5 +2,5 @@ require "epathway_scraper"
 
 scraper = EpathwayScraper.scrape_and_save(
   "https://online.unley.sa.gov.au/ePathway/Production",
-  list_type: :last_30_days
+  list_type: :last_30_days, state: "SA"
 )
