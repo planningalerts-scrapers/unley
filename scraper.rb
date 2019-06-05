f0b1f114da8ce96ac9edfdcd6d8ea18ac6a828ea
Matthew Landauer <mlandauer@gmail.com>
2019-06-05 13:54:32 +1000

    Update to latest version of gem

--- scraper.rb
+++ scraper.rb
@@ -1,6 +1,3 @@
 require "epathway_scraper"
 
-scraper = EpathwayScraper.scrape_and_save(
-  "https://online.unley.sa.gov.au/ePathway/Production",
-  list_type: :last_30_days, state: "SA"
-)
+EpathwayScraper.scrape_and_save_authority(:unley)
